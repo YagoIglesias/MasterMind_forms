aaf9cd4ee4793051337128e06844183e587f0e97
YagoIglesias <yago.iglesias@eduvaud.ch>
2023-12-28 17:28:26 +0100

    Final

---
+++ MasterMind_forms/Instructions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasterMind_forms
+{
+    public partial class instructions : Form
+    {
+        public instructions()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Main play = new Main();
+            play.Show();
+        }
+    }
+}
--- MasterMind_forms/Main.cs
+++ MasterMind_forms/Main.cs
@@ -16,70 +16,71 @@ using System.Windows.Forms;
 
 namespace MasterMind_forms
 {
-    public partial class Main : Form
+ 
+    public partial class  Main : Form
     {
      
         // conteur de position de label
-        int countLabel = 1;
+        int _countLabel = 1;
 
         //conteur d'essais
-        int tries = 1;
+        int _tries = 1;
 
         // couleurs possibles
-        const int MAX_COLORS = 7;
+        const int _MAX_COLORS = 7;
 
         // tableau de couleurs avec les couleurs possibles
-        Color[] tabColors = new Color[MAX_COLORS];
+        //Color[] _tabColors = new Color[_MAX_COLORS];
 
 
         // constante choix des couleurs
-        const int ROWS = 10 ;
-        const int COLUMNS = 4 ;
+        const int _ROWS = 10 ;
+        const int _COLUMNS = 4 ;
         // tableau de labels pour le choix de couleurs de l'utilisateur
-        Label[,] tabLblChoice = new Label[COLUMNS, ROWS];
+        Label[,] _tabLblChoice = new Label[_COLUMNS, _ROWS];
 
         // separation des label et panel 
-        int margin = 20;
-        int padding = 20;
+        int _margin = 20;
+        int _padding = 20;
 
         // tableau de label pour verifier les couleures correctes ou fauses
-        Label[,] tabLblCheck = new Label[COLUMNS, ROWS];
+        Label[,] _tabLblCheck = new Label[_COLUMNS, _ROWS];
 
         // crer le code couleur aleatoire
-        Random random = new Random();
+        Random _random = new Random();
 
         // variables index tableau
         // columns
-        int rowIndex = 0;
+        int _rowIndex = 0;
         //lignes
-        int columnIndex = 0;
+        int _columnIndex = 0;
 
         // constante couleur aleatoire
-        const int MAX_RANDOM = 4;
+        const int _MAX_RANDOM = 4;
 
         // tableau couleur utilisateur
-        Color[,] tabUserColor = new Color[COLUMNS,ROWS];
+        Color[,] _tabUserColor = new Color[_COLUMNS, _ROWS];
 
         // conteur couleurs bonne possition
-        int goodPosition = 0;
+        int _goodPosition = 0;
 
         // conteur couleures mauvaise possition
-        int badPosition = 0;
+        int _badPosition = 0;
 
         //couleurs possibles returnes de Colors()
-        Color[] returnedFromColors;
+        Color[] _returnedFromColors;
 
         // stocker le return du RandomCode()
-        Color[] returnedFromRandomCode;
+        Color[] _returnedFromRandomCode;
 
         // tableau pour comparer les couleurs
-        Color[] copyFromCode = new Color[MAX_RANDOM];
+        Color[] _copyFromCode = new Color[_MAX_RANDOM];
 
         // copy des chois de l'utilisateur
-        Color[,] userCopyChoice = new Color[COLUMNS, ROWS];
+        Color[,] _userCopyChoice = new Color[_COLUMNS, _ROWS];
 
         // variable bool pour activer ou desactiver le code secret
-        bool enable = false;
+        bool _enable = false;
 
         // var stocker la hauteur des label crées pour le choix des couleurs
         int _heigthLblChoice = 0;
@@ -110,10 +111,10 @@ namespace MasterMind_forms
         Color[] RandomCode(Color[] tabColors)
         {
             //code aleatoire entre 7 couleurs
-            int color1 = random.Next(tabColors.Length);
-            int color2 = random.Next(tabColors.Length);
-            int color3 = random.Next(tabColors.Length);
-            int color4 = random.Next(tabColors.Length);
+            int color1 = _random.Next(tabColors.Length);
+            int color2 = _random.Next(tabColors.Length);
+            int color3 = _random.Next(tabColors.Length);
+            int color4 = _random.Next(tabColors.Length);
             //tableau de random
             Color[] randomCode = { tabColors[color1], tabColors[color2], tabColors[color3], tabColors[color4] };
             // returner random code
@@ -128,25 +129,25 @@ namespace MasterMind_forms
         public void Game()
         {
             // compteur essaies 
-            tries = 1;
-            lblNbTries.Text = tries.ToString();
+            _tries = 1;
+            lblNbTries.Text = _tries.ToString();
             // masquer le bouton quitter
             btnExit.Visible = false;
             // masquer btn rejouer
             btnReplay.Visible = false;
             //couleurs possibles
-            returnedFromColors = Colors();
+            _returnedFromColors = Colors();
             // code aleatoire
-            RandomCode(returnedFromColors);
+            RandomCode(_returnedFromColors);
             // stocker le return du randomCode
-            returnedFromRandomCode = RandomCode(returnedFromColors);
+            _returnedFromRandomCode = RandomCode(_returnedFromColors);
             // copy du tableau
-            returnedFromRandomCode.CopyTo(copyFromCode, 0);
+            _returnedFromRandomCode.CopyTo(_copyFromCode, 0);
             // attribuer la couleur aleatoire aux label afin d'avoir le mode debeugue
-            lblCode1.BackColor = returnedFromRandomCode[0];
-            lblCode2.BackColor = returnedFromRandomCode[1];
-            lblCode3.BackColor = returnedFromRandomCode[2];
-            lblCode4.BackColor = returnedFromRandomCode[3];
+            lblCode1.BackColor = _returnedFromRandomCode[0];
+            lblCode2.BackColor = _returnedFromRandomCode[1];
+            lblCode3.BackColor = _returnedFromRandomCode[2];
+            lblCode4.BackColor = _returnedFromRandomCode[3];
             // masquer les labels avec le code
             lblCode1.Hide();
             lblCode2.Hide();
@@ -155,50 +156,50 @@ namespace MasterMind_forms
 
 
             // tableau de labels pour la selection des couleures par l'utilisateur
-            for (int i = 0; i < ROWS; i++)
+            for (int i = 0; i < _ROWS; i++)
             {
-                for (int j = 0; j < COLUMNS; j++)
+                for (int j = 0; j < _COLUMNS; j++)
                 {
                     // creation des proprietes du label
                     Label choice = new Label();
                     //nom 
-                    choice.Name = "lblChoice" + countLabel++;
+                    choice.Name = "lblChoice" + _countLabel++;
                     // style
                     choice.BackColor = Color.Transparent;
                     choice.BorderStyle = BorderStyle.Fixed3D;
                     //taille
                     choice.Size = new Size(30, 30);
                     // position
-                    choice.Location = new Point((choice.Width * j) + (j * padding), (choice.Height * i) + (i * margin + padding));
+                    choice.Location = new Point((choice.Width * j) + (j * _padding), (choice.Height * i) + (i * _margin + _padding));
                     //associer au panel
                     choicePanel.Controls.Add(choice);
                     // ajouter les btns au tableau
-                    tabLblChoice[j, i] = choice;
+                    _tabLblChoice[j, i] = choice;
                     // stocker la hauteur des label
                     _heigthLblChoice = 30;
                 }
 
             }
             // tableau de labels pour verifier la position correcte
-            for (int k = 0; k < ROWS; k++)
+            for (int k = 0; k < _ROWS; k++)
             {
-                for (int l = 0; l < COLUMNS; l++)
+                for (int l = 0; l < _COLUMNS; l++)
                 {
                     // creation des proprietes du label
                     Label check = new Label();
                     //nom 
-                    check.Name = "lblCeck" + countLabel++;
+                    check.Name = "lblCeck" + _countLabel++;
                     // style
                     check.BackColor = Color.Transparent;
                     check.BorderStyle = BorderStyle.Fixed3D;
                     //taille
                     check.Size = new Size(25, 25);
                     // position
-                    check.Location = new Point((check.Width * l) + (l * (padding - 10)), ((check.Height * k) + (_heigthLblChoice / 2 + 8) + (k * (margin + 5))));
+                    check.Location = new Point((check.Width * l) + (l * (_padding - 10)), ((check.Height * k) + (_heigthLblChoice / 2 + 8) + (k * (_margin + 5))));
                     //associer au panel
                     checkPanel.Controls.Add(check);
                     // ajouter les label de check au tableau
-                    tabLblCheck[l, k] = check;
+                    _tabLblCheck[l, k] = check;
                 }
             }
         }
@@ -210,20 +211,21 @@ namespace MasterMind_forms
         /// <param name="e"></param>
         private void Colors_Click(object sender, EventArgs e)
         {
+            // evenements des btns de couleurs 
             Button clickedButton = (Button)sender;
             //remplir la ligne
-            if (columnIndex == COLUMNS)
+            if (_columnIndex == _COLUMNS)
             {
                 MessageBox.Show("La ligne est remplie");
             }
             else
             {
-                tabLblChoice[columnIndex, rowIndex].BackColor = clickedButton.BackColor;
+                _tabLblChoice[_columnIndex, _rowIndex].BackColor = clickedButton.BackColor;
                 // stocker la couleur selectione par l'utilisateur
-                tabUserColor[columnIndex, rowIndex] = clickedButton.BackColor;
+                _tabUserColor[_columnIndex, _rowIndex] = clickedButton.BackColor;
                 // copier le code couleur de l'utilisateur
-                userCopyChoice[columnIndex,rowIndex] = clickedButton.BackColor;
-                columnIndex++;
+                _userCopyChoice[_columnIndex, _rowIndex] = clickedButton.BackColor;
+                _columnIndex++;
             }
              
 
@@ -238,31 +240,31 @@ namespace MasterMind_forms
         private void btnCheck_Click(object sender, EventArgs e)
         {
             // reinitialiser les couleurs du tableau de copie
-            for (int i = 0; i < MAX_RANDOM; i++)
+            for (int i = 0; i < _MAX_RANDOM; i++)
             {
-                copyFromCode[i] = returnedFromRandomCode[i];
+                _copyFromCode[i] = _returnedFromRandomCode[i];
 
             }
             
             // parcourir la ligne 
-            for (int i = 0; i < COLUMNS; i++)
+            for (int i = 0; i < _COLUMNS; i++)
             {
                 // verifier les couleurs entres par l'utilisateur dans la ligne
-                if (returnedFromRandomCode[i] == tabUserColor[i,rowIndex])
+                if (_returnedFromRandomCode[i] == _tabUserColor[i, _rowIndex])
                 {
                     // remplacer les couleurs trouves par du gris
-                    copyFromCode[i] = Color.Gray;
+                    _copyFromCode[i] = Color.Gray;
                     // remplacer les couleur correctes par du beige
-                    userCopyChoice[i,rowIndex] = Color.Beige;
+                    _userCopyChoice[i, _rowIndex] = Color.Beige;
                     // si c'est correct afficher une case en blanc
-                    tabLblCheck[goodPosition, rowIndex].BackColor = Color.White; 
+                    _tabLblCheck[_goodPosition, _rowIndex].BackColor = Color.White; 
                     // si couleur correct incrementer 
-                    goodPosition++;
+                    _goodPosition++;
                     // si tout est correct alors gagne
-                    if (goodPosition == 4)
+                    if (_goodPosition == 4 || _goodPosition == 4 && _tries == 11)
                     {
                         // message de victoire
-                        MessageBox.Show("Félicitation, vous avez gagnés !!", "Victoire");
+                        MessageBox.Show("Félicitation, vous avez gagné !!", "Victoire", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                         // rendre visible le btn quitter
                         btnExit.Visible = true;
                         // rendre vible le btn rejouer
@@ -275,48 +277,42 @@ namespace MasterMind_forms
                         btnMagenta.Enabled = false;
                         btnOrange.Enabled = false;
                         btnRed.Enabled = false;
-                        btnWhite.Enabled = false;
-
+                        break;
                     }
-                    
+
                 }
                 // assigner la valeur de good possition a bad possition
-                badPosition = goodPosition;
+                _badPosition = _goodPosition;
                 
             }
-            for (int i = 0; i < COLUMNS; i++)
+            for (int i = 0; i < _COLUMNS; i++)
             {
                 // verifier le code secret sans les couleurs deja trouves
-                for (int j = 0; j < copyFromCode.Length; j++)
+                for (int j = 0; j < _copyFromCode.Length; j++)
                 {
-                    if (userCopyChoice[i, rowIndex] == copyFromCode[j] && i != j)
+                    if (_userCopyChoice[i, _rowIndex] == _copyFromCode[j] && i != j)
                     {
                         // changer la couleur si on trouve une couleur mal place afin d'eviter les doublons
-                        copyFromCode[j] = Color.Gray;
+                        _copyFromCode[j] = Color.Gray;
                         // afficher les case noir pour les couleurs mal places
-                        tabLblCheck[badPosition, rowIndex].BackColor = Color.Black;
-                        badPosition++;
+                        _tabLblCheck[_badPosition,_rowIndex  ].BackColor = Color.Black;
+                        _badPosition++;
                         break;
                     } 
                 }
-
             }
-
             // reinitialiser la colonne
-            columnIndex = 0;
+            _columnIndex = 0;
             // incrementer la ligne
-            rowIndex++;
-            // compteur reinitialiser
-            goodPosition = 0;
-            // compteur reinitialise
-            badPosition = 0;
+            _rowIndex++;
             // incrementer les essais
-            tries++;
+            _tries++;
            // afficher les essais
-            lblNbTries.Text = tries.ToString();
-            if (tries == 11)
-            {
-                MessageBox.Show("Vous avez utilise vos 10 essais !", "Perdu");
+            lblNbTries.Text = _tries.ToString();
+            // si le joueur ne trouve pas le code secret avent les 10 essai il a perdu !
+            if (_goodPosition != 4 && _tries == 11)
+            {        
+                MessageBox.Show("Vous avez utilisé vos 10 essais !", "Perdu", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 // rendre visible le btn quitter
                 btnExit.Visible = true;
                 // rendre vible le btn rejouer
@@ -329,8 +325,12 @@ namespace MasterMind_forms
                 btnOrange.Enabled = false;
                 btnRed.Enabled = false;
                 btnWhite.Enabled = false;
-
             }
+            // compteur reinitialiser
+            _goodPosition = 0;
+            // compteur reinitialise
+            _badPosition = 0;
+
         }
 
         /// <summary>
@@ -352,15 +352,15 @@ namespace MasterMind_forms
         private void btnDelete_Click(object sender, EventArgs e)
         {
             // si les case sont vides on ne peut pas effacer
-            if (columnIndex == 0 )
+            if (_columnIndex == 0 )
             {
 
             }
             // si elles sont remplies ont peut effacer
             else
             {
-                columnIndex--;
-                tabLblChoice[columnIndex, rowIndex].BackColor = Color.Transparent;
+                _columnIndex--;
+                _tabLblChoice[_columnIndex, _rowIndex].BackColor = Color.Transparent;
             }
             
             
@@ -374,7 +374,7 @@ namespace MasterMind_forms
         private void btnExit_Click(object sender, EventArgs e)
         {
             // message de remerciment 
-            MessageBox.Show("Merci d'avoir participe !");
+            MessageBox.Show("Merci d'avoir participé !", "Au revoir");
             // fin du programme
             Application.Exit();
         }
@@ -390,8 +390,8 @@ namespace MasterMind_forms
             choicePanel.Controls.Clear();
             checkPanel.Controls.Clear();
             //compeur lignes et colonnes
-            rowIndex = 0;
-            columnIndex = 0;
+            _rowIndex = 0;
+            _columnIndex = 0;
             // rejoue
             Game();
             // Reactivation des boutons
@@ -412,7 +412,7 @@ namespace MasterMind_forms
         private void btnDebug_Click(object sender, EventArgs e)
         {
             // si les labels sont desactivées on les montres
-            if (enable == false)
+            if (_enable == false)
             {
                 // activer le code
                 lblCode1.Show();
@@ -420,7 +420,7 @@ namespace MasterMind_forms
                 lblCode3.Show();
                 lblCode4.Show();
                 // code activée
-                enable = true;
+                _enable = true;
             }
             // si ils sont actives on les cache
             else
@@ -431,7 +431,7 @@ namespace MasterMind_forms
                 lblCode3.Hide();
                 lblCode4.Hide();
                 // code désactivée
-                enable = false;
+                _enable = false;
             }
            
 
